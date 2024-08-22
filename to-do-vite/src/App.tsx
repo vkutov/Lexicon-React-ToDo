bf77c1c014d4867afe9af600a604718dea67abeb
vw rabbit <vkutov@gmail.com>
2024-08-22 11:17:54 +0200

    first

---
+++ to-do-vite/src/App.tsx
@@ -0,0 +1,13 @@
+  import React from 'react';
+  import './styles.css';
+  import TodoList from './TodoList.js';
+
+  const App = () => {
+      return (
+          <>
+              <TodoList />
+          </>
+      );
+  };
+
+  export default App;
---
+++ to-do-vite/src/TodoList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <h2>Hello World </h2>
+    </div>
+  );
+};
+
+export default TodoList;
+  
